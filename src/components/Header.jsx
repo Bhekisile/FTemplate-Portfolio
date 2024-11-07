96dc46485797965e9dacf09af5780adf499d6dd0
Bhekisile <bhekisilejozi@gmail.com>
2024-11-07 12:35:56 +0200

    Connect Router to the corresponding pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,26 +2,24 @@ import "../styles/Header.scss";
 import linkedIn from "../assets/linkedIn.png";
 import twitter from "../assets/twitter.png";
 import github from "../assets/githu.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className='container container-fluid'>
-      <nav className="secondary-nav">
-        <ul className='nav-left'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Recent Work</li>
-          <li>Skills</li>
-          <li>Contact</li>
-        </ul>
-        <ul className='nav-right'>
-          <li><img src={linkedIn} alt="LinkedIn icon" /></li>
-          <li><img src={github} className="length" alt="Github icon"></img></li>
-          <li><img src={twitter} className="length" alt="Twitter icon" /></li>
-        </ul>
-      </nav>
-      
-    </div>
+    <nav className="secondary-nav">
+      <ul className='nav-left'>
+        <Link to='/' ><li>Home</li></Link>
+        <Link to='/about'><li>About</li></Link>
+        <Link to='/projects'><li>Recent Work</li></Link>
+        <Link to='skills'><li>Skills</li></Link>
+        <Link to='/contact'><li>Contact</li></Link>
+      </ul>
+      <ul className='nav-right'>
+        <li><img src={linkedIn} alt="LinkedIn icon" /></li>
+        <li><img src={github} className="length" alt="Github icon"></img></li>
+        <li><img src={twitter} className="length" alt="Twitter icon" /></li>
+      </ul>
+    </nav>      
   )
 }
 
