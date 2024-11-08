e3607cbb0e5a6f61da498e04c6d05c8c5db95360
Bhekisile <bhekisilejozi@gmail.com>
2024-11-08 22:42:43 +0200

    Add data for projects

---
+++ src/assets/projectsData.js
@@ -0,0 +1,47 @@
+const projectsData = [
+  {
+    projectName: 'ProShop',
+    projectDescription: 'This is an e-commerce website built with MERN stack. It is an end-to-end application with a seamless user experience and interactions. The customers can search for the product and purchase it online. The admin manages the application and has CRUD permission. It authorizes and authenticates users, allowing them to place an order after authentication and have access to their own account.',
+    imageUrl: 'https://res.cloudinary.com/dgimm1mwi/image/upload/v1731097095/proshop_nxlyno.png',
+    projectUrl: 'https://proshop-q9dt.onrender.com/',
+    linkSource: 'https://github.com/Bhekisile/proshop',
+  },
+  {
+    projectName: 'Workout App',
+    projectDescription: 'This is a metric website built with React, CSS, and Jest for tests. It contains data or parameters used for stock screening and displays a list of companies selected from all over the world with the corresponding parameters and values. The user can filter the application by company\'s name. It is more beneficial to those who search for stocks. The data is extracted from the Financial Modeling Prep API.',
+    imageUrl: 'https://res.cloudinary.com/dgimm1mwi/image/upload/v1731097095/Exercise-rails_oh7bdu.png',
+    projectUrl: 'https://metricsstock.netlify.app/',
+    linkSource: 'https://github.com/Bhekisile/Metrics-webapp',
+  },
+  {
+    projectName: 'Recipe App',
+    projectDescription: 'The project is built with Ruby on Rails, a PostgreSQL database, and Bootstrap for styling. It is about keeping track of all recipes and ingredients for the users. The users can save ingredients, keep track of what they have and create recipes, generate a shopping list based on what they have, and record the missing gradients. Also, since sharing recipes is an important part of cooking, the recipes can be made public so anyone can access them.',
+    imageUrl: 'https://res.cloudinary.com/dgimm1mwi/image/upload/v1731097095/recipeApp_vjmham.png',
+    projectUrl: 'https://recipe-app-8lwd.onrender.com',
+    linkSource: 'https://github.com/Bhekisile/Recipe-app',
+  },
+  
+  {
+    projectName: 'Space Travelers app',
+    projectDescription: 'Space Travelers Hub is built with React, CSS, and Jest for testing. It is a web application designed to assist users in managing their rocket and mission reservations for space travel. The application aims to provide a platform where users can easily book and keep track of their reserved rockets and missions. Additionally, it offers a profile page feature where users can view the details of their reserved rockets and missions. This kind of application could be useful for space enthusiasts, scientists, or anyone interested in space travel.',
+    imageUrl: 'https://res.cloudinary.com/dgimm1mwi/image/upload/v1731097524/spaceHub_z7ykkp.png',
+    projectUrl: 'https://spacetravelershub0x.netlify.app/',
+    linkSource: 'https://github.com/Bupilipili/Space-Travelers-Hub',
+  },
+  {
+    projectName: 'Complex App',
+    projectDescription: 'The project is built with Node.js, Express, MongoDB, SendGrid, and Socket.io. It enables users to create posts, chat, send email, and follow each other. The user must log in, in order to have access to creating and modifying the post. The user can chat with other users and be able to read the posts of those he follows.',
+    imageUrl: 'https://res.cloudinary.com/dgimm1mwi/image/upload/v1731097523/complexApp_erfaia.png',
+    projectUrl: 'https://jscoursecomplex-app.onrender.com',
+    linkSource: 'https://github.com/Bhekisile/Complex-app',
+  },
+  {
+    projectName: 'Natours',
+    projectDescription: 'The project is built with WordPress and is about university activities. It contains lecturers and the modules related to them. The students can register their credentials and start using this app. Liking of the professors is available only for the login user.',
+    imageUrl: 'https://res.cloudinary.com/dgimm1mwi/image/upload/v1731097096/Natours_ihkl44.png',
+    projectUrl: 'http://fictional-university.local/',
+    linkSource: 'https://github.com/Bhekisile/Fictional-University',
+  }
+];
+
+export default projectsData;
